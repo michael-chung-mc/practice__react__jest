f2f2b4f5439f905b613a0f2a3e83573d0eb50672
michael-chung-mc <chung.michael.mc@gmail.com>
2023-06-02 20:21:13 -0400

    Complete Step 2: module & factories

---
+++ script.js
@@ -0,0 +1,14 @@
+const gameBoard = (() => {
+    let gameboard = [[],[],[]];
+    return {
+        gameboard
+    }
+})();
+
+const playerFactory = () => {
+
+};
+
+const gameManager = (() => {
+
+})();
