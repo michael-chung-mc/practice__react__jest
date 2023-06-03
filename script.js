c928460224dce7ce4b2fa8dd90ecd11c3595e4c9
michael-chung-mc <chung.michael.mc@gmail.com>
2023-06-02 20:44:33 -0400

    Complete Step 3: Render Contents of Gameboard

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const gameBoard = (() => {
-    let gameboard = [[],[],[]];
+const board = (() => {
+    let grid = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]];
+    function getBoard () {
+        return grid;
+    }
+    function setValue (value, x, y) {
+        grid[x][y] = value;
+    }
     return {
-        gameboard
+        getBoard,
+        setValue
     }
 })();
 
@@ -9,6 +16,25 @@ const playerFactory = () => {
 
 };
 
-const gameManager = (() => {
-
+const manager = (() => {
+    function updateScreen() {
+        let gameboard = board.getBoard();
+        let contentDiv = document.getElementById("content_container");
+        for (let i = 0; i < gameboard.length; i++) {
+            let rowDiv = document.createElement("div");
+            rowDiv.setAttribute("id", "board_row");
+            for (let j = 0; j < gameboard[i].length; j++) {
+                let cell = document.createElement("div");
+                cell.setAttribute("id", "board_cell");
+                cell.innerHTML = gameboard[i][j];
+                rowDiv.appendChild(cell);
+            }
+            contentDiv.appendChild(rowDiv);
+        }
+    }
+    return {
+        updateScreen
+    }
 })();
+
+manager.updateScreen();
