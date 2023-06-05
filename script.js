c71d0f31773f121c9509dc8d698a00cb458b5513
michael-chung-mc <chung.michael.mc@gmail.com>
2023-06-05 10:53:03 -0400

    Fix alternating of player when clicking invalid cell

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const board = (() => {
     let marks = [" ", "O", "X"];
-    let grid = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]];
+    let grid = [[marks[0],marks[0],marks[0]],[marks[0],marks[0],marks[0]],[marks[0],marks[0],marks[0]]];
     function getBoard () {
         return grid;
     }
@@ -14,60 +14,69 @@ const board = (() => {
         return marks[2];
     }
     function getColumnState() {
-        if (grid[0][0] == grid[0][1] == grid[0][2])
+        if (grid[0][0] === grid[0][1] === grid[0][2])
         {
+            console.log("0,0 - 0,1 - 0,2");
             return grid[0][0];
         }
-        else if (grid[1][0] == grid[1][1] == grid[1][2])
+        else if (grid[1][0] === grid[1][1] === grid[1][2])
         {
+            console.log("1,0 - 1,1 - 1,2");
             return grid[1][0];
         }
-        else if (grid[2][0] == grid[2][1] == grid[2][2])
+        else if (grid[2][0] === grid[2][1] === grid[2][2])
         {
+            console.log("2,0 - 2,1 - 2,2");
             return grid[2][0];
         }
         return marks[0];
     }
     function getRowState() {
-        if (grid[0][0] == grid[1][0] == grid[2][0])
+        if (grid[0][0] === grid[1][0] === grid[2][0])
         {
+            console.log("0,0 - 1,0 - 2,0");
             return grid[0][0];
         }
-        else if (grid[0][1] == grid[1][1] == grid[2][1])
+        else if (grid[0][1] === grid[1][1] === grid[2][1])
         {
+            console.log("0,1 - 1,1 - 2,1");
             return grid[0][1];
         }
-        else if (grid[0][2] == grid[1][2] == grid[2][2])
+        else if (grid[0][2] === grid[1][2] === grid[2][2])
         {
+            console.log("0,2 - 1,2 - 2,2");
             return grid[0][2];
         }
         return marks[0];
     }
     function getDiagonalState () {
-        if (grid[0][0] == grid[1][1] == grid [2][2])
+        if (grid[0][0] === grid[1][1] === grid[2][2])
         {
+            console.log("0,0 - 1,1 - 2,2");
             return grid[0][0];
         }
-        else if (grid[0][2] == grid[1][1] == grid [2][0])
+        else if (grid[0][2] === grid[1][1] === grid[2][0])
         {
+            console.log("0,2 - 1,1 - 2,0");
             return grid[0][2];
         }
         return marks[0];
     }
     function getState () {
+        console.log(grid);
         let rowWin = getRowState();
         let columnWin = getColumnState();
         let diagonalWin = getDiagonalState();
         if (rowWin != marks[0]) {
-            console.log(rowWin + "won the row")
+            console.log(rowWin + " won the row")
             return rowWin;
         }
         else if (columnWin != marks[0]) {
-            console.log(columnWin + "won the column")
+            console.log(columnWin + " won the column")
             return columnWin;
         }
         else if (diagonalWin != marks[0]) {
-            console.log(diagonalWin + "won diagonally")
+            console.log(diagonalWin + " won diagonally")
             return diagonalWin;
         }
         else {
@@ -75,20 +84,20 @@ const board = (() => {
         }
     }
     function setValue (mark, x, y) {
-        console.log("Setting Value:");
-        console.log(grid);
-        console.log(mark);
-        console.log(x);
-        console.log(y);
-        console.log(marks);
+        // console.log("Setting Value:");
+        // console.log(grid);
+        // console.log(mark);
+        // console.log(x);
+        // console.log(y);
+        // console.log(marks);
         if (grid[x][y] == marks[0])
         {
-            console.log("position" + x + "," + y + " is marked");
+            // console.log("position" + x + "," + y + " is marked");
             grid[x][y] = mark;
             return true;
         }
         else {
-            console.log("position" + x + "," + y + " is occupied");
+            // console.log("position" + x + "," + y + " is occupied");
             return false;
         }
     }
@@ -110,17 +119,21 @@ const manager = (() => {
     let currentPlayer = 1;
     function placeMarker (x, y) {
         return () => {
-            console.log("Placing Mark:");
-            console.log(x);
-            console.log(y);
-            console.log(currentPlayer);
+            // console.log("Placing Mark:");
+            // console.log(x);
+            // console.log(y);
+            // console.log(currentPlayer);
             if (currentPlayer == 1) {
-                board.setValue(board.getPlayerOneMark(), x, y);
-                currentPlayer = 2;
+                if (board.setValue(board.getPlayerOneMark(), x, y))
+                {
+                    currentPlayer = 2;
+                }
             }
             else {
-                board.setValue(board.getPlayerTwoMark(), x, y);
-                currentPlayer = 1;
+                if (board.setValue(board.getPlayerTwoMark(), x, y))
+                {
+                    currentPlayer = 1;
+                }
             }
             updateScreen();
         }
